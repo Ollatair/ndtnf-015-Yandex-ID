c91c736042cd51d394b4065cebba19b5c54f5b29
Andrey Tikhonenkov <ollatair@gmail.com>
2024-07-15 18:18:57 +0300

    add yandex ID

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const YandexStrategy = require('passport-yandex').Strategy;
+
+const app = express();
+ 
+app.set('view engine', 'ejs');
+ 
+app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.use(new YandexStrategy({
+    clientID: '855883cbeec648cab3c1eb9731ddcaa3',
+    clientSecret: 'b3d6a3f20e704953bc4c5d61c7068922',
+    callbackURL: "http://localhost:3000/login/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+ 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+// Главная страница
+app.get('/', (req, res) => {
+  res.render('index', { user: req.user });
+});
+
+// Страница профиля
+app.get('/profile', ensureAuthenticated, (req, res) => {
+  res.render('profile', { user: req.user });
+});
+
+// Аутентификация с помощью Яндекс
+app.get('/login',
+  passport.authenticate('yandex'));
+
+app.get('/login/callback', 
+  passport.authenticate('yandex', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/profile');
+  });
+
+// Выход из системы
+app.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+  
+
+app.post('/participate', ensureAuthenticated, (req, res) => {
+    req.user.participating = !req.user.participating;
+    res.redirect('/profile');
+  });
+
+// Middleware для проверки аутентификации
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
+
+// Запуск сервера
+app.listen(3000, () => {
+  console.log('Сервер запущен на http://localhost:3000');
+});
